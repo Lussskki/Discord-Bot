17405c8f9c15764f6c63f3e69b9cd1c0e12cf845
Luka Guledani <lukaguledani123@gmail.com>
2025-01-03 20:16:47 +0400

    implement in the index slashes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,44 @@
 import { Client, GatewayIntentBits } from 'discord.js'
+
 import tutorialMap from './tutorialMap.js'
-import { config } from './config.js'
 import errorMap from './errorMap.js'
+import { registerSlashCommands } from './slashCommand.js'
+import dotenv from 'dotenv'
+
+dotenv.config() // Load environment variables from .env file
+
+const clientId = process.env.CLIENT_ID
+const guildId = process.env.GUILD_ID
+const token = process.env.DISCORD_TOKEN
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 })
 
-client.once('ready', () => {
+client.once('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`)
+
+  // Register slash commands
+  await registerSlashCommands(clientId, guildId, token)
 })
 
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return
+
+  const { commandName } = interaction
+
+  // Handle commands
+  if (commandName === 'ping') {
+    await interaction.reply('Gamovida')
+  } else if (commandName === 'hell') {
+    await interaction.reply('It\'s working!!')
+  } else {
+    await interaction.reply('Unknown command!')
+  }
+})
+
+
+// Message-based command handling
 client.on('messageCreate', message => {
   if (message.author.bot) return
 
@@ -43,4 +71,4 @@ client.on('messageCreate', message => {
 })
 
 // Use your environment variable for the bot token
-client.login(process.env.DISCORD_TOKEN)
+client.login(token)
