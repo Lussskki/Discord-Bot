e1aec7adb2468cda1a9149256f96b0a07e8e836b
Lussskki <lussskki@gmail.com>
2025-01-06 12:24:24 +0400

    remove tutorialMap.js

--- backend/src/slashCommands/tutorialMap.js
+++
@@ -1,9 +0,0 @@
-const tutorialMap = {   sayhello: () => {
-  return [`Hello, I'm Botzilla, Please note that you can only communicate with me using slashes,
-I'm currently working on it and you'll see the results in the near future..`
-        ]
-      }
-  }
-  
-
-  export default tutorialMap
