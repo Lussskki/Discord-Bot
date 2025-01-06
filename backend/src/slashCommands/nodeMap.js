0d63488450d3ae5f77abfeb38f9986ea3f680dbf
Lussskki <lussskki@gmail.com>
2025-01-06 12:22:16 +0400

    rename a tutorialMap.js

---
+++ backend/src/slashCommands/nodeMap.js
@@ -0,0 +1,11 @@
+const tutorialMap = {   sayhello: () => {
+  return [
+`Hello, I'm Botzilla, Please note that you can only communicate with me using slashes,
+i can help you with node.js tasks and errors, 
+currently working on it and you'll see the results in the near future.`
+        ]
+      }
+  }
+  
+
+  export default tutorialMap
