0778219ad0a9956de9ab2c55ed721dcdb6a696d9
Lussskki <lussskki@gmail.com>
2025-01-09 15:36:13 +0400

    refactor config

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 
-export const config={
+export const config = {
+    clientId: process.env.CLIENT_ID,
+    guildId: process.env.GUILD_ID,
     token: process.env.DISCORD_TOKEN
 }
